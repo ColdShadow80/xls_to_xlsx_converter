7ae885db0b69f4378b5ac0372cc580faf8258127
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2024-09-23 12:27:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def convert_xls_to_xlsx():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5950)
